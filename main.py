9d4f28c7f6236f62d984db3ac71a3ea127d8fa47
Domagoj Glavačević <lodi432@users.noreply.github.com>
2024-12-27 08:14:44 +0100

    Initial commit: Add script to print Oppenheimer's friends

---
+++ main.py
@@ -0,0 +1,18 @@
+# List of Dr. Oppenheimer's friends
+oppenheimer_friends = [
+    "Albert Einstein",
+    "Niels Bohr",
+    "Ernest Lawrence",
+    "Richard Feynman",
+    "Hans Bethe",
+    "Edward Teller",
+    "Enrico Fermi",
+    "John von Neumann"
+]
+
+# Print a title
+print("Dr. Oppenheimer's Friends:")
+
+# Loop through the list and print each friend's name
+for friend in oppenheimer_friends:
+    print(friend)
